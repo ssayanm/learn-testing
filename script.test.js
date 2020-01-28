6b0a434cdef25266b690d3c90d786fa1344a11e1
Sayan Mukherjee <hello@sayanmukherjee.com>
2020-01-28 09:44:15 +0530

    adding upodated files

--- script.test.js
+++ script.test.js
@@ -2,12 +2,23 @@ const googleSearch = require("./script");
 
 dbMock = ["dog.com", "aflowers.com", "banimals.com", "dogimages.com"];
 
-it("this is a silly test", () => {
-  expect("hello").toBe("hello");
-  //googleSearch("testssss", dbMock);
-});
+describe("googleSearch", () => {
+  it("this is a silly test", () => {
+    expect("hello").toBe("hello");
+    //googleSearch("testssss", dbMock);
+  });
 
-it("is searching duckduckdo", () => {
-  expect(googleSearch("testtest", dbMock)).toEqual([]);
-  expect(googleSearch("d", dbMock)).toEqual(["dog.com", "dogimages.com"]);
+  it("is searching duckduckdo", () => {
+    expect(googleSearch("testtest", dbMock)).toEqual([]);
+    expect(googleSearch("d", dbMock)).toEqual(["dog.com", "dogimages.com"]);
+  });
+
+  it("work with undefined and null input", () => {
+    expect(googleSearch(undefined, dbMock)).toEqual([]);
+    expect(googleSearch(null, dbMock)).toEqual([]);
+  });
+
+  it("does not return more than 2 matches", () => {
+    expect(googleSearch(".com", dbMock).length).toEqual(2);
+  });
 });
